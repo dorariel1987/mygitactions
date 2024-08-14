07f8922ef7c095ebff3d25daa20a5c32ce04d6d4
dorariel1987 <64736310+dorariel1987@users.noreply.github.com>
2024-08-14 15:38:13 +0300

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -3,17 +3,20 @@ main.py
 A simple script to print greetings and farewells.
 """
 
+
 def hello():
     """
     Prints a greeting message.
     """
     print("hi")
 
+
 def bye():
     """
     Prints a farewell message.
     """
     print("bye")
+
 
 if __name__ == "__main__":
     hello()
